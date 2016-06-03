346221101c235c08601e29ce95bc544ab38d5650
Ryan Ballenger <ballenger@g.harvard.edu>
2016-06-03 00:29:16 -0400

    Initial Commit

---
+++ onTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onTheMap
+//
+//  Created by Ryan Ballenger on 6/3/16.
+//  Copyright © 2016 Ryan Ballenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
